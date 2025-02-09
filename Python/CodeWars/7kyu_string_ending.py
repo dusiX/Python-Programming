49fb6af79d17be539497e0968cfa1e7e0e9e1b81
Klaudia "Dusia" Koch <102617702+dusiX@users.noreply.github.com>
2025-02-09 17:48:30 +0100

    Update 7kyu_string_ending.py

--- Python/CodeWars/7kyu_string_ending.py
+++ Python/CodeWars/7kyu_string_ending.py
@@ -1,2 +1,9 @@
+# Complete the solution so that it returns true if the first argument(string) passed in ends with the 2nd argument (also a string).
+
+# Examples:
+
+# solution('abc', 'bc') # returns true
+# solution('abc', 'd') # returns false
+
 def solution(text, ending):
     return False if text.find(ending, len(text)-len(ending)) == -1 else True
