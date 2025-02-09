06ee5e43205e22ddb88a76cba4e5f72c2b8e426b
Klaudia "Dusia" Koch <102617702+dusiX@users.noreply.github.com>
2025-02-09 17:34:14 +0100

    Update 6kyu_break_camelCase.py

--- Python/CodeWars/6kyu_break_camelCase.py
+++ Python/CodeWars/6kyu_break_camelCase.py
@@ -1,3 +1,10 @@
+# Complete the solution so that the function will break up camel casing, using a space between words.
+
+# Example
+# "camelCasing"  =>  "camel Casing"
+# "identifier"   =>  "identifier"
+# ""             =>  ""
+
 def solution(s):
     final = s
     for i in range(len(s)):
