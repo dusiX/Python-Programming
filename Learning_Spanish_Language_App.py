5bc6b4c309e300f1c56ec06c59182a5d7c90a271
Klaudia "Dusia" Koch <102617702+dusiX@users.noreply.github.com>
2025-04-14 11:13:21 +0200

    Update and rename Learning_Language_App.py to Learning_Spanish_Language_App.py
    Updating name and fixing some bugs

--- Learning_Spanish_Language_App.py
+++ Learning_Spanish_Language_App.py
@@ -152,11 +152,35 @@ class MainWindow(QWidget):
         self.setLayout(layout)
 
     def action1(self):
-        column = 2
-        row = randint(1, sheet.max_row)
+        learn_words = []
+        repeat_words = []
+        learned_words = []
 
-        word_to_translate = get_value(column, row)
-        translated_word = get_value(1, row)
+        for row in range(2, sheet.max_row + 1):
+            status = sheet.cell(row, 4).value
+            if status == "DO NAUCZENIA":
+                learn_words.append(row)
+            elif status == "DO POWTÓRKI":
+                repeat_words.append(row)
+            elif status == "NAUCZONE":
+                learned_words.append(row)
+
+        category = randint(1, 100)
+        if category <= 60 and learn_words:
+            chosen_row = learn_words[randint(0, len(learn_words) - 1)]
+        elif category <= 90 and repeat_words:
+            chosen_row = repeat_words[randint(0, len(repeat_words) - 1)]
+        elif learned_words:
+            chosen_row = learned_words[randint(0, len(learned_words) - 1)]
+        elif repeat_words:
+            chosen_row = repeat_words[randint(0, len(repeat_words) - 1)]
+        elif learn_words:
+            chosen_row = learn_words[randint(0, len(learn_words) - 1)]
+        else:
+            chosen_row = randint(2, sheet.max_row)
+
+        word_to_translate = get_value(2, chosen_row)
+        translated_word = get_value(1, chosen_row)
         dialog = InputDialog("Przetłumacz słowo z polskiego na hiszpański","Słowo do przetłumaczenia: \n\n" + word_to_translate + "\n\nPodaj przetłumaczone słowo:", self)
         if dialog.exec():
             if translated_word == dialog.get_input():
@@ -164,18 +188,46 @@ class MainWindow(QWidget):
             else:
                 result_message = "Błędna odpowiedź! Prawidłowa odpowiedź to:\n\n" + translated_word
             
-            result_dialog = ResultDialog(result_message, self)
-            result_dialog.exec()
+            result_dialog = ResultDialog(result_message + "\n\nCzy chcesz spróbować następnego słowa?", self)
 
-        if not(dialog.get_input()):
-            set_value(row, translated_word == dialog.get_input())
+            result_dialog.buttons.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
+
+            set_value('D', chosen_row, translated_word == dialog.get_input())
+
+            if result_dialog.exec() == QDialog.DialogCode.Accepted:
+                self.action1()
+
 
     def action2(self):
-        column = 1
-        row = randint(1, sheet.max_row)
+        learn_words = []
+        repeat_words = []
+        learned_words = []
 
-        word_to_translate = get_value(column, row)
-        translated_word = get_value(2, row)
+        for row in range(1, sheet.max_row + 1):
+            status = sheet.cell(row, 3).value
+            if status == "DO NAUCZENIA":
+                learn_words.append(row)
+            elif status == "DO POWTÓRKI":
+                repeat_words.append(row)
+            elif status == "NAUCZONE":
+                learned_words.append(row)
+
+        category = randint(1, 100)
+        if category <= 60 and learn_words:
+            chosen_row = learn_words[randint(0, len(learn_words) - 1)]
+        elif category <= 90 and repeat_words:
+            chosen_row = repeat_words[randint(0, len(repeat_words) - 1)]
+        elif learned_words:
+            chosen_row = learned_words[randint(0, len(learned_words) - 1)]
+        elif repeat_words:
+            chosen_row = repeat_words[randint(0, len(repeat_words) - 1)]
+        elif learn_words:
+            chosen_row = learn_words[randint(0, len(learn_words) - 1)]
+        else:
+            chosen_row = randint(1, sheet.max_row)
+
+        word_to_translate = get_value(1, chosen_row)
+        translated_word = get_value(2, chosen_row)
         dialog = InputDialog("Przetłumacz słowo z hiszpańskiego na polski","Słowo do przetłumaczenia: \n\n" + word_to_translate + "\n\nPodaj przetłumaczone słowo:", self)
         if dialog.exec():
             if translated_word == dialog.get_input():
@@ -183,33 +235,37 @@ class MainWindow(QWidget):
             else:
                 result_message = "Błędna odpowiedź! Prawidłowa odpowiedź to:\n\n" + translated_word
             
-            result_dialog = ResultDialog(result_message, self)
-            result_dialog.exec()
+            result_dialog = ResultDialog(result_message + "\n\nCzy chcesz spróbować następnego słowa?", self)
 
-        if not(dialog.get_input()):
-            set_value(row, translated_word == dialog.get_input())
+            result_dialog.buttons.setStandardButtons(QDialogButtonBox.StandardButton.Ok)
+
+            set_value('C', chosen_row, translated_word == dialog.get_input())
+
+            if result_dialog.exec() == QDialog.DialogCode.Accepted:
+                self.action2()
 
     def action3(self):
-        new_polish_word_adress = 'B' + str(sheet.max_row + 1)
-        new_spanish_word_adress = 'A' + str(sheet.max_row + 1)
-        status = 'C' + str(sheet.max_row + 1)
+        row = sheet.max_row + 1
+        new_polish_word_adress = 'B' + str(row)
+        new_spanish_word_adress = 'A' + str(row)
 
         dialog = WordEntryDialog(self)
         if dialog.exec():
             spanish_word, polish_translation = dialog.get_inputs()
 
-        if not(dialog.get_inputs()):
+        if dialog.get_inputs():
             sheet[new_polish_word_adress] = polish_translation
             sheet[new_spanish_word_adress] = spanish_word
-            sheet[status] = 'DO NAUCZENIA'
+            sheet['C' + str(row)] = 'DO NAUCZENIA'
+            sheet['D' + str(row)] = 'DO NAUCZENIA'
 
         wb.save(file_path)
 
 def get_value(column, row):
     return sheet.cell(row, column).value
 
-def set_value(row, correct):
-    adress = 'C' + str(row)
+def set_value(column, row, correct):
+    adress = column + str(row)
     if correct == True:
         match(sheet[adress].value):
             case 'NAUCZONE':
